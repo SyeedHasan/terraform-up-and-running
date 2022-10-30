baade61734ca1f79caaf73c726967bfbc6757928
Syed Hasan <syed.hasan@careem.com>
2022-10-30 12:33:35 +0500

    Chapter 2

---
+++ Code/basic-aws.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "MyInstance" {
+    ami           = "ami-09d3b3274b6c5d4aa"
+    instance_type = "t2.micro"
+    tags = {
+      "Name" = "TerraformBasicInstance"
+    }
+}
